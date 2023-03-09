7bd9aea5998b8ed19ac16ff805b646ec7f1fbdd6
John Requizo <jr.jemerson14@gmail.com>
2023-03-09 23:03:55 +1100

    Complete challenge-1

--- set-1/challenge-1/challenge-1.go
+++ set-1/challenge-1/challenge-1.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/base64"
 	"fmt"
 )
 
 func main() {
 	hexString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	byteString := []byte(hexString)
-	fmt.Printf("mystr:\t %v \n", byteString)
+	fmt.Printf("String to Encode:\n%s\n", hexString)
+	fmt.Printf("String length %d\n\n", len(hexString))
 	decodedString, err := hex.DecodeString(hexString)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Decoded string:\t %v \n", decodedString)
+	fmt.Printf("String in bytes:\n%v\n", decodedString)
+	fmt.Printf("Byte length: %d\n\n", len(decodedString))
+
+	base64String := base64.RawStdEncoding.EncodeToString(decodedString)
+	fmt.Printf("Base 64 String: %s\n", base64String)
 }
