a0a6506af724a746c78526fecbdc3198601867fa
John Requizo <jereq1@student.monash.edu>
2023-03-10 20:42:00 +1100

    Completed challenge 3

--- set-1/challenge-3/challenge-3.go
+++ set-1/challenge-3/challenge-3.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"encoding/hex"
+	"fmt"
+)
 
+const englishCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+
+func main() {
+	hexString := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	byteString, err := hex.DecodeString(hexString)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(englishCharacters); i++ {
+		fmt.Printf("%c:\t", englishCharacters[i])
+		charByte := byte(englishCharacters[i])
+		cBytes := make([]byte, len(byteString))
+		for j := 0; j < len(byteString); j += 1 {
+			xoredBytes := charByte ^ byteString[j]
+			fmt.Printf("%c", xoredBytes)
+			cBytes = append(cBytes, xoredBytes)
+		}
+		fmt.Println("")
+	}
 }
